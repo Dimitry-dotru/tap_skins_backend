b7cfcac5d8b81bfe4d271550ac6b8c5c4610e79c
Dmitry Shumskiy <ejedima260@gmail.com>
2024-07-08 14:51:03 +0300

    --added: connect to telegram

---
+++ src/bot/index.ts
@@ -0,0 +1,14 @@
+import {bot, frontendLink} from "../index";
+
+bot.start((ctx) => {
+  ctx.reply(
+    "This is test CS bot application for development, this message will change due to deploy",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Press me", web_app: { url: frontendLink } }],
+        ],
+      },
+    }
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
 dotenv.config({ path: "./.env" });
 
-const PORT = process.env.port;
+const { serverPort, botToken, frontendLink } = process.env;
 const app = express();
+const bot = new Telegraf(botToken);
 
-app.listen(PORT, () => {
-  console.log("Server running in port " + PORT);
-})
+app.listen(serverPort, () => {
+  console.log("Server running in port " + serverPort);
+  bot.launch();
+});
+
+export { bot, frontendLink };
+import "./bot/index";
