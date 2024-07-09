f98d1c841195d651eef0e8160cb16039a6542344
Dmitry Shumskiy <ejedima260@gmail.com>
2024-07-09 12:00:59 +0300

    --added: connect to db + data models

---
+++ src/config/mongoose.ts
@@ -0,0 +1,59 @@
+import { mongoose } from "../index";
+
+const weaponSchemaTemplate = {
+  item_id: String,
+  name: String,
+  rarity: String,
+  price: Number,
+  float: Number,
+  weapon_type: String,
+  weapon_name: String,
+  startrack: Boolean,
+  image_src: String,
+}
+
+//! USER
+const userSchema = new mongoose.Schema({
+  experience: Number,
+  balance_common: Number,
+  ballance_purple: Number,
+  user_id: Number,
+  last_daily_bonus_time_clicked: Number,
+  invited_users: Number,
+});
+const userModel = mongoose.model("users", userSchema);
+
+//! TASKS
+const tasksCompletedSchema = new mongoose.Schema({
+  tasks_completed: [{
+    task_id: String,
+    date_completed: Number
+  }],
+  user_id: Number
+});
+const tasksCompletedModel = mongoose.model("tasks_completed", userSchema);
+
+const taskSchema = new mongoose.Schema({
+  reward: Number,
+  name: String,
+  icon: String,
+  type: String,
+  link_to_join: String,
+  task_id: String
+});
+const taskModel = mongoose.model("tasks", userSchema);
+
+
+
+//! HISTORY
+const historySchema = new mongoose.Schema({
+  items: [{
+    ...weaponSchemaTemplate,
+    status: String,
+    link_to_trade: String,
+  }],
+  user_id: Number
+});
+const historyModel = mongoose.model("history", userSchema);
+
+export { userModel };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
 import { Telegraf } from "telegraf";
+import mongoose from "mongoose";
+import { userModel } from "./config/mongoose";
+
 dotenv.config({ path: "./.env" });
 
-const { serverPort, botToken, frontendLink } = process.env;
+const { serverPort, botToken, frontendLink, mongoUrl } = process.env;
 const app = express();
 const bot = new Telegraf(botToken);
 
-app.listen(serverPort, () => {
-  console.log("Server running in port " + serverPort);
-  bot.launch();
-});
-
-export { bot, frontendLink };
 import "./bot/index";
+import "./config/mongoose";
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to db successfully");
+    app.listen(serverPort, async () => {
+
+
+      console.log("Server running in port " + serverPort);
+      bot.launch();
+
+      // const newUser = new userModel({
+      //   experience: 0,
+      //   balance_common: 0,
+      //   ballance_purple: 0,
+      //   id: 623165387,
+      //   last_daily_bonus_time_clicked: 0,
+      //   invited_users: 0,
+      // });
+
+      // await newUser.save();
+    });
+  })
+  .catch((err) => console.log("Error to connect to db!", err));
+
+export { bot, frontendLink, mongoose };
