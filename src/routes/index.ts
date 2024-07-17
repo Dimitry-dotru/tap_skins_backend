94ad181c09a47420296395b56ded2c926dc149e0
Dmitry Shumskiy <ejedima260@gmail.com>
2024-07-17 18:20:27 +0300

    ...

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import { authUser, userSubscription } from "./callbacks";
 app.post("/auth", authUser);
 app.post("/subscription", userSubscription);
 
+app.get("/test-req", (req, res) => {
+  return res.send("Hello from server-side backend. Weirdo");
+});
 
 app.post("/log", (req, res) => {
   console.log("\n\nReceived:...");
