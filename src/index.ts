84cb2756c8239ce05a0fbe8d287b38d4d46e3afd
Dmitry Shumskiy <ejedima260@gmail.com>
2024-07-17 18:43:45 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ import { onConnect } from "./config/websocket";
 
 dotenv.config({ path: "./.env" });
 
-const { serverPort, botToken, frontendLink, mongoUrl, webSocketPort } = process.env;
+const { serverPort, botToken, frontendLink, mongoUrl, webSocketPort } =
+  process.env;
 const app = express();
 const bot = new Telegraf(botToken);
 const server: HTTPServer = createServer(app);
@@ -29,18 +30,18 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log("Connected to db successfully");
-    app.listen(serverPort, async () => {
-      console.log("Server running in port " + serverPort);
-      bot.launch();
-      wss.on("connection", onConnect);
-    });
-  })
-  .catch((err) => console.log("Error to connect to db!", err));
+// mongoose
+//   .connect(mongoUrl)
+//   .then(() => {
+//     console.log("Connected to db successfully");
+//   })
+//   .catch((err) => console.log("Error to connect to db!", err));
 
+app.listen(serverPort, async () => {
+  console.log("Server running in port " + serverPort);
+  bot.launch();
+  wss.on("connection", onConnect);
+});
 import "./config/bot";
 import "./config/mongoose";
 import "./config/websocket";
