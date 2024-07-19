10469684a51bdd456ccebd296c249530b5e9bc25
Dmitry Shumskiy <ejedima260@gmail.com>
2024-07-19 13:11:25 +0300

    --updated: cors testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,9 @@ import { authUser, userSubscription } from "./callbacks";
 
 app.post("/auth", authUser);
 app.post("/subscription", userSubscription);
-
+app.get("/test-req", (req, res) => {
+  return res.send("Everything is okay");
+});
 
 app.post("/log", (req, res) => {
   console.log("\n\nReceived:...");
